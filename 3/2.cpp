578c9c229fddb0455c7a95a6cf6dc4fc8b954f78
kuckjwi <rnrghks09@gmail.com>
2021-08-26 10:16:58 +0900

    modified chap3 2.cpp

--- 3/2.cpp
+++ 3/2.cpp
@@ -2,27 +2,22 @@
 
 using namespace std;
 
-int n, m, k;
-vector<int> v;
-
 int main() {
-    // N, M, K를 공백을 기준으로 구분하여 입력 받기
-    cin >> n >> m >> k;
+    int n = 5;
+    int m = 8;
+    int k = 3;
 
-    // N개의 수를 공백을 기준으로 구분하여 입력 받기
-    for (int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-        v.push_back(x);
-    }
+    int src[] = { 2,4,5,4,6 };
+ 
+    vector<int> v(src, src + sizeof(src) / sizeof(int));
 
     sort(v.begin(), v.end()); // 입력 받은 수들 정렬하기
+    
     int first = v[n - 1]; // 가장 큰 수
     int second = v[n - 2]; // 두 번째로 큰 수
 
     // 가장 큰 수가 더해지는 횟수 계산
-    int cnt = (m / (k + 1)) * k;
-    cnt += m % (k + 1);
+    int cnt = (m / (k + 1)) * k + m % (k + 1);
 
     int result = 0;
     result += cnt * first; // 가장 큰 수 더하기
